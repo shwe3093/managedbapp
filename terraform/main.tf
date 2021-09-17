98a6905848969303b4bfbd11f92e3ca07b836a8e
root <root@ip-10-0-1-156.us-west-2.compute.internal>
2021-09-17 08:04:22 +0000

    Added rds policy to ec2 role

--- terraform/main.tf
+++ terraform/main.tf
@@ -89,6 +89,80 @@ resource "aws_iam_role" "ec2_role" {
       ]
     })
   }
+  
+  inline_policy {
+    name = "rds-access"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+	  Action = [
+	    "rds:*",
+            "application-autoscaling:DeleteScalingPolicy",
+            "application-autoscaling:DeregisterScalableTarget",
+            "application-autoscaling:DescribeScalableTargets",
+            "application-autoscaling:DescribeScalingActivities",
+            "application-autoscaling:DescribeScalingPolicies",
+            "application-autoscaling:PutScalingPolicy",
+            "application-autoscaling:RegisterScalableTarget",
+            "cloudwatch:DescribeAlarms",
+            "cloudwatch:GetMetricStatistics",
+            "cloudwatch:PutMetricAlarm",
+            "cloudwatch:DeleteAlarms",
+            "ec2:DescribeAccountAttributes",
+            "ec2:DescribeAvailabilityZones",
+            "ec2:DescribeCoipPools",
+            "ec2:DescribeInternetGateways",
+            "ec2:DescribeLocalGatewayRouteTables",
+            "ec2:DescribeLocalGatewayRouteTableVpcAssociations",
+            "ec2:DescribeLocalGateways",
+            "ec2:DescribeSecurityGroups",
+            "ec2:DescribeSubnets",
+            "ec2:DescribeVpcAttribute",
+            "ec2:DescribeVpcs",
+            "ec2:GetCoipPoolUsage",
+            "sns:ListSubscriptions",
+            "sns:ListTopics",
+            "sns:Publish",
+            "logs:DescribeLogStreams",
+            "logs:GetLogEvents",
+            "outposts:GetOutpostInstanceTypes"
+	  ]
+	  Effect = "Allow",
+	  Resource: "*"
+        },
+	{
+            Action: "pi:*",
+            Effect: "Allow",
+            Resource: "arn:aws:pi:*:*:metrics/rds/*"
+        },
+	{
+            Action: "iam:CreateServiceLinkedRole",
+            Effect: "Allow",
+            Resource: "*",
+            Condition: {
+                StringLike: {
+                    "iam:AWSServiceName": [
+                        "rds.amazonaws.com",
+                        "rds.application-autoscaling.amazonaws.com"
+                    ]
+                }
+            }
+        },
+	{
+	    Effect: "Allow",
+	    Action: [
+                "rds-db:connect"
+            ],
+            Resource: [
+                "arn:aws:rds-db:us-west-2:${local.account_id}:dbuser:*/*"
+            ]
+	}
+         
+      ]
+    })
+  }
 
   inline_policy {
     name = "ecr-access"
